3560b17ff637642d3fc350c61b916773048b57fb
Danny Lamarti <lamarti.danny@gmail.com>
2017-12-17 14:01:37 +0100

    updates gradle build

--- src/main/java/io/lamart/reduxtream/Util.java
+++ src/main/java/io/lamart/reduxtream/Util.java
@@ -1,19 +1,12 @@
 package io.lamart.reduxtream;
 
 
-import java.util.AbstractMap;
-import java.util.Map;
-
 import io.reactivex.functions.BiFunction;
 
 public final class Util {
 
     private Util() {
         throw new Error();
-    }
-
-    public static <K, V> Map.Entry<K, V> newEntry(K key, V value) {
-        return new AbstractMap.SimpleImmutableEntry<K, V>(key, value);
     }
 
     public static <T> BiFunction<T, Object, T> wrapReducers(final Iterable<BiFunction<T, Object, T>> reducers) {
